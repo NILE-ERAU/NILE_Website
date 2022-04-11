b2851b1eab27d3d65aa0ac2c0ba38dba0a44076d
NILE-ERAU <nile.erau@gmail.com>
2022-04-10 22:15:37 -0700

    config

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'Trolley123!');
-define('DB_NAME', 'nile_robot');
+define('DB_NAME', 'NILE');
 
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
