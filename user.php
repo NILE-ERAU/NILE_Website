92b47173d536fdb6970688cc064b6ec669bd0f4f
NILE-ERAU <nile.erau@gmail.com>
2022-04-10 23:04:30 -0700

    default user

---
+++ user.php
@@ -0,0 +1,26 @@
+<?php
+/* Include the database connection script. */
+include 'config.php';
+/* Username. */
+$username = 'NILE';
+/* Password. */
+$password = 'mache123';
+/* Secure password hash. */
+$hash = password_hash($password, PASSWORD_DEFAULT);
+/* Insert query template. */
+$query = 'INSERT INTO system_info (id, username, password) VALUES (1, :name, :passwd)';
+/* Values array for PDO. */
+$values = [':name' => $username, ':passwd' => $hash];
+/* Execute the query. */
+try
+{
+  $res = $pdo->prepare($query);
+  $res->execute($values);
+}
+catch (PDOException $e)
+{
+  /* Query error. */
+  echo 'Query error.';
+  die();
+}
+?>
