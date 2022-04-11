4ed7a882044ea445a9651ca359ee66f7513f8fd3
NILE-ERAU <nile.erau@gmail.com>
2022-04-10 23:06:06 -0700

    user

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 
 // Include config filexx
 require_once "config.php";
+require_once "user.php";
 
 // Define variables and initialize with empty values
 $username = $password = "";
