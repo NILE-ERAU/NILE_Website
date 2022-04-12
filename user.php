4c0b04a6ef74c5526532cc58a4284cfa83fe7791
NILE-ERAU <nile.erau@gmail.com>
2022-04-11 22:40:49 -0700

    users2

--- user.php
+++ user.php
@@ -7,11 +7,13 @@ $username = 'NILE';
 $password = 'mache123';
 /* Secure password hash. */
 $hash = password_hash($password, PASSWORD_DEFAULT);
+
+$role = 'admin'
 /* Execute the query. */
-$sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+$sql = "INSERT INTO users (username, password, role) VALUES (?, ?, ?)";
 if($stmt = mysqli_prepare($link, $sql)){
   // Bind variables to the prepared statement as parameters
-  mysqli_stmt_bind_param($stmt, "ss", $username, $hash);
+  mysqli_stmt_bind_param($stmt, "sss", $username, $hash, $role);
   if(mysqli_stmt_execute($stmt)){
       // Redirect to login page
   } else{
