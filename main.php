82455ffe1c90bbca1b150e646d7d99158a912d2c
NILE-ERAU <nile.erau@gmail.com>
2022-04-11 23:53:04 -0700

    logout

--- main.php
+++ main.php
@@ -23,6 +23,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 <body>
 <img src="/images/NILE_corner.png" alt="NILE Triangle Logo" style="width:400px">
-<p>Logged in as <?php echo $_SESSION["role"]; ?></p>
+<p>Logged in as <?php echo $_SESSION["role"]; ?>   <a href="https://nilerobot.info/logout.php" title="Logout">Logout</a></p>
 </body>
 </html>
