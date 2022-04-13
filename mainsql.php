0d412fb5ab160eaf805487e623e770aa522b58a2
NILE-ERAU <nile.erau@gmail.com>
2022-04-13 03:07:09 -0700

    completed commands

--- mainsql.php
+++ mainsql.php
@@ -2,6 +2,7 @@
 include("config.php");
 $result = mysqli_query($link, "select * FROM queued_commands ORDER BY timestamp ASC");
 
+echo "<h2>Queued Commands</h2>";
 echo "<table class= 'greyGridTable' >";
 echo "<thead>";
 echo "<tr>";
@@ -22,4 +23,33 @@ while($data = mysqli_fetch_row($result))
 };
 echo "</tbody>";
 echo "</table>";
+
+$result = mysqli_query($link, "select * FROM completed_commands ORDER BY timestamp ASC");
+echo "<br/>";
+echo "<h2>Completed Commands</h2>";
+echo "<table class= 'greyGridTable' >";
+echo "<thead>";
+echo "<tr>";
+echo "<td>ID</td>";
+echo "<td>Completed At</td>";
+echo "<td>Queued At</td>";
+echo "<td>Command</td>";
+echo "<td>Argument</td>";
+echo "<td>Status</td></tr></thead>";
+echo "<tbody>";
+while($data = mysqli_fetch_row($result))
+{
+    echo "<tr>";
+    echo "<td align=center>$data[0]</td>";
+    echo "<td align=center>$data[1]</td>";
+    echo "<td align=center>$data[2]</td>";
+    echo "<td align=center>$data[3]</td>";
+    $arg_str = '';
+    echo "<td align=center>$arg_str</td>";
+    echo "<td align=center>$data[13]</td>";
+    echo "</tr>";
+};
+echo "</tbody>";
+echo "</table>";
+
 ?>
