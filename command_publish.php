3cd6456a3562f10e51a23c06e6a2d0058cf08ab6
NILE-ERAU <nile.erau@gmail.com>
2022-04-13 23:25:03 -0700

    form

--- command_publish.php
+++ command_publish.php
@@ -2,6 +2,7 @@
 
 
 echo "<h2>Schedule Command</h2>";
+echo "<form action='add_entry.php'><tr>";
 echo "<table class= 'greyGridTable' >";
 echo "<thead>";
 echo "<tr>";
@@ -14,7 +15,6 @@ echo "<td>D0</td>";
 echo "<td>D1</td>";
 echo "<td>I0</td></tr></thead>";
 echo "<tbody>";
-echo "<form action='add_entry.php'><tr>";
 echo "<td><input type='datetime-local' id='timestamp' name='timestamp'></td>";
 echo "<td><input type='text' id='command' name='command'></td>";
 echo "<td><input type='number' id='theta' name='theta'></td>";
@@ -22,9 +22,9 @@ echo "<td><input type='number' id='r' name='r'></td>";
 echo "<td><input type='number' id='z' name='z'></td>";
 echo "<td><input type='number' id='d0' name='d0'></td>";
 echo "<td><input type='number' id='d1' name='d1'></td>";
-echo "<td><input type='number' id='i0' name='i0'></td></tr></form>";
+echo "<td><input type='number' id='i0' name='i0'></td></tr>";
 echo "</tbody>";
-echo "</table>";
+echo "</table></form>";
 
 
 ?>
