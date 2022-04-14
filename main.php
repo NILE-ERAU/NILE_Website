ad01a0eb694d54674ec996ffd155c3cf0d89ad89
NILE-ERAU <nile.erau@gmail.com>
2022-04-13 22:04:40 -0700

    equals

--- main.php
+++ main.php
@@ -34,7 +34,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
       String role = "<?php echo $_SESSION['role'];?>";
       var timer, delay = 10000;
       //var timer, delay = 10000;
-      if (role.matches("admin")==true) {
+      if (role.equals("admin")==true) {
         var timer, delay = 1000;
       }
 
