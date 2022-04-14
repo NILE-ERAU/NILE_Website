98c86d23d083d98d2b7505ecb3a34c44f4c1ed83
NILE-ERAU <nile.erau@gmail.com>
2022-04-13 21:42:51 -0700

    row

--- command_publish.php
+++ command_publish.php
@@ -14,7 +14,7 @@ echo "<td>D0</td>";
 echo "<td>D1</td>";
 echo "<td>I0</td></tr></thead>";
 echo "<tbody>";
-echo "<form action='add_entry.php'><tr>";
+echo "<tr><form action='add_entry.php'>";
 echo "<td><input type='datetime-local' id='timestamp' name='timestamp'></td>";
 echo "<td><input type='text' id='command' name='command'></td>";
 echo "<td><input type='number' id='theta' name='theta'></td>";
@@ -22,8 +22,7 @@ echo "<td><input type='number' id='r' name='r'></td>";
 echo "<td><input type='number' id='z' name='z'></td>";
 echo "<td><input type='number' id='d0' name='d0'></td>";
 echo "<td><input type='number' id='d1' name='d1'></td>";
-echo "<td><input type='number' id='i0' name='i0'></td></tr>";
-echo "</form>";
+echo "<td><input type='number' id='i0' name='i0'></td></form></tr>";
 echo "</tbody>";
 echo "</table>";
 
