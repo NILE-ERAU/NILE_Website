896357497196ab3dca52ed00b9a1abd3dd9604eb
NILE-ERAU <nile.erau@gmail.com>
2022-04-13 20:43:32 -0700

    form

--- mainsql.php
+++ mainsql.php
@@ -2,12 +2,40 @@
 include("config.php");
 $result = mysqli_query($link, "select * FROM queued_commands ORDER BY timestamp ASC");
 
+if(strcmp($_SESSION['role'],'admin')) {
+  echo "<h2>Schedule Command</h2>";
+  echo "<table class= 'greyGridTable' >";
+  echo "<thead>";
+  echo "<tr>";
+  echo "<td>Time</td>";
+  echo "<td>Command</td>";
+  echo "<td>θ</td>";
+  echo "<td>R</td>";
+  echo "<td>Z</td>";
+  echo "<td>D0</td>";
+  echo "<td>D1</td>";
+  echo "<td>I0</td></tr></thead>";
+  echo "<tbody>";
+  echo "<form action='add_entry.php'>"
+  echo "<td><input type='datetime-local' id='timestamp' name='timestamp></td>";
+  echo "<td><input type='text' id='command' name='command'></td>";
+  echo "<td><input type='number' id='theta' name='theta'></td>";
+  echo "<td><input type='number' id='r' name='r'></td>";
+  echo "<td><input type='number' id='z' name='z'></td>";
+  echo "<td><input type='number' id='d0' name='d0'></td>";
+  echo "<td><input type='number' id='d1' name='d1'></td>";
+  echo "<td><input type='number' id='i0' name='i0'></td></tr></thead>";
+  echo "</form>";
+  echo "</tbody>";
+  echo "</table>";
+}
+
 echo "<h2>Queued Commands</h2>";
 echo "<table class= 'greyGridTable' >";
 echo "<thead>";
 echo "<tr>";
-echo "<td>ID</td>";
 echo "<td>Time</td>";
+echo "<td>Command</td>";
 echo "<td>Command</td>";
 echo "<td>Argument</td></tr></thead>";
 echo "<tbody>";
