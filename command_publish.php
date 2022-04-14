f24b67833c44ab631993bea3d1fbd7097242a49a
NILE-ERAU <nile.erau@gmail.com>
2022-04-13 23:43:27 -0700

    add button

--- command_publish.php
+++ command_publish.php
@@ -13,7 +13,8 @@ echo "<td>R</td>";
 echo "<td>Z</td>";
 echo "<td>D0</td>";
 echo "<td>D1</td>";
-echo "<td>I0</td></tr></thead>";
+echo "<td>I0</td>";
+echo "<td>Add</td></tr></thead>";
 echo "<tbody>";
 echo "<td><input type='datetime-local' id='timestamp' name='timestamp'></td>";
 echo "<td><input type='text' id='command' name='command'></td>";
@@ -23,6 +24,7 @@ echo "<td><input type='number' id='z' name='z'></td>";
 echo "<td><input type='number' id='d0' name='d0'></td>";
 echo "<td><input type='number' id='d1' name='d1'></td>";
 echo "<td><input type='number' id='i0' name='i0'></td></tr>";
+echo "<td align=center><input type='submit' value='+'></td>";
 echo "</tbody>";
 echo "</table></form>";
 
