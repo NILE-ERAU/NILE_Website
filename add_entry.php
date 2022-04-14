162143ac441ba55a4426daee27141b35fdf6b267
NILE-ERAU <nile.erau@gmail.com>
2022-04-14 00:20:06 -0700

    add_entry

--- add_entry.php
+++ add_entry.php
@@ -1,11 +1,19 @@
 <?php
 include("config.php");
+$timestamp = trim($_POST["timestamp"]);
+$timestamp_sql = str_replace("T"," ",$timestamp)  . ":00.000000";
+$command = trim($_POST["command"]);
+$theta = trim($_POST["theta"]);
+$r = trim($_POST["r"]);
+$z = trim($_POST["z"]);
+$d0 = trim($_POST["d0"]);
+$d1 = trim($_POST["d1"]);
+$i0 = trim($_POST["i0"]);
 $sql =  "insert INTO queued_commands
          (timestamp, command, pos_theta_q, pos_r_q, pos_z_q, argument_d0, argument_d1, argument_i0)
          VALUES
-        (" . $_POST["timestamp"] . "," . $_POST["command"]. "," . $_POST["theta"]. ","
-           . $_POST["r"]. "," . $_POST["z"]. "," . $_POST["d0"]. "," . $_POST["d1"]. ","
-           . $_POST["d0"] . ");";
+        (" . $timestamp_sql . "," . $command . "," . $theta . "," . $r . ","
+           . $z . "," . $d0 . "," . $d1 . "," . $i0 . ");";
 
 if ($link->query($sql) === TRUE) {
  echo "New record created successfully";
