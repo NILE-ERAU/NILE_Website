71debbf2299bdab5beedc0ed2acfb4cdcd3f381a
NILE-ERAU <nile.erau@gmail.com>
2022-04-14 20:49:13 -0700

    ''

--- command_publish.php
+++ command_publish.php
@@ -15,10 +15,9 @@ echo "<td>D0</td>";
 echo "<td>D1</td>";
 echo "<td>I0</td>";
 echo "<td>Add</td></tr></thead>";
-<<<<<<< HEAD
 echo "<tbody>";
 echo "<td><input type='datetime-local' id='timestamp' name='timestamp'></td>";
-echo "<td><input type='text' id='command' name='command' width=10%></td>";
+echo "<td><input type='text' id='command' name='command' width='10%'></td>";
 echo "<td><input type='number' id='theta' name='theta' value=0 step='any'></td>";
 echo "<td><input type='number' id='r' name='r' value=0 step='any'></td>";
 echo "<td><input type='number' id='z' name='z' value=0 step='any'></td>";
@@ -26,18 +25,6 @@ echo "<td><input type='number' id='d0' name='d0' value=0 step='any'></td>";
 echo "<td><input type='number' id='d1' name='d1' value=0 step='any'></td>";
 echo "<td><input type='number' id='i0' name='i0' value=0></td>";
 echo "<td><input type='submit' value='+'></td>";
-=======
-echo "<tbody><tr>";
-echo "<td width = 250px><input type='datetime-local' id='timestamp' name='timestamp'></td>";
-echo "<td width = 100px><input type='text' id='command' name='command'></td>";
-echo "<td><input type='number' id='theta' name='theta' value=0 step='any' class='numinput'></td>";
-echo "<td><input type='number' id='r' name='r' value=0 step='any' class='numinput'></td>";
-echo "<td><input type='number' id='z' name='z' value=0 step='any' class='numinput'></td>";
-echo "<td><input type='number' id='d0' name='d0' value=0 step='any' class='numinput'></td>";
-echo "<td><input type='number' id='d1' name='d1' value=0 step='any' class='numinput'></td>";
-echo "<td><input type='number' id='i0' name='i0' value=0 class='numinput'></td>";
-echo "<td><input type='submit' value='+' class='numinput'></td>";
->>>>>>> 615f1f8455c70c9170d2922e21a554301743d06c
 echo "</tr></tbody>";
 echo "</table></form>";
 
