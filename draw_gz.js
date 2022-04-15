b785ff87a2ce5a8cbdeb8efdfc6b0750ebd33e75
NILE-ERAU <nile.erau@gmail.com>
2022-04-15 00:31:31 -0700

    800

--- draw_gz.js
+++ draw_gz.js
@@ -2,5 +2,5 @@ let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
 
 canvas.style.width = '100%';
-let width = canvas.width;
-let height = canvas.width * 0.75;
+let width = canvas.width = 800;
+let height = canvas.height = 800;
