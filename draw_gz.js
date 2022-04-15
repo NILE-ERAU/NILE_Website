a98f49e14eb6c1c6d7c3d769da4869e5415db799
NILE-ERAU <nile.erau@gmail.com>
2022-04-15 00:40:12 -0700

    cavas

--- draw_gz.js
+++ draw_gz.js
@@ -1,5 +1,5 @@
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 
 ctx.fillStyle = 'green';
 ctx.fillRect(10, 10, 150, 100);
