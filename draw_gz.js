ef49ca58afc885827d54bc4d9e34b05344e75bd4
NILE-ERAU <nile.erau@gmail.com>
2022-04-15 00:42:36 -0700

    camvas

--- draw_gz.js
+++ draw_gz.js
@@ -1,5 +1,7 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+let canvas = document.querySelector('#canvas');
+if(canvas.getContext) {
+   let ctx = main.getContext('2d');
+}
 
 ctx.fillStyle = 'green';
 ctx.fillRect(10, 10, 150, 100);
