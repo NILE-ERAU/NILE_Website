405b387af23a7a780b2240e469c1a2a50725affc
NILE-ERAU <nile.erau@gmail.com>
2022-04-15 00:29:00 -0700

    float

--- main.php
+++ main.php
@@ -169,7 +169,7 @@ $role = $_SESSION['role'];
     <p style ="text-align: right;">Logged in as <?php echo $_SESSION["role"]; ?>   <a href="http://nilerobot.info/logout.php" title="Logout">Logout</a></p>
   </div>
   </div>
-<div>
+<div width=100% float="center">
 <canvas id="canvas"></canvas>
 </div>
 
