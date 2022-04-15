1587166936c7e71ca62056a3826d95bb71faab66
nphaklides <nphaklides@hotmail.com>
2022-04-14 17:01:58 -0700

    interior form

--- command_publish.php
+++ command_publish.php
@@ -2,8 +2,8 @@
 
 
 echo "<h2>Schedule Command</h2>";
-echo "<form action='add_entry.php' method='post' width=100%>";
 echo "<table class= 'greyGridTable' >";
+echo "<form action='add_entry.php' method='post' width=100%>";
 echo "<thead>";
 echo "<tr>";
 echo "<td>Time</td>";
@@ -26,7 +26,7 @@ echo "<td><input type='number' id='d1' name='d1' value=0 step='any'></td>";
 echo "<td><input type='number' id='i0' name='i0' value=0></td>";
 echo "<td><input type='submit' value='+'></td>";
 echo "</tr></tbody>";
-echo "</table></form>";
+echo "</form></table>";
 
 
 ?>
