58e7f627657fac425188e67001db41c00a2c2b65
NILE-ERAU <nile.erau@gmail.com>
2022-04-16 20:45:29 -0700

    td

--- command_publish.php
+++ command_publish.php
@@ -17,7 +17,7 @@ echo "<td>I0</td>";
 echo "<td>Add</td></tr></thead>";
 echo "<tbody>";
 echo "<td><input type='datetime-local' id='timestamp' name='timestamp'></td>";
-echo "<td><input list='command' id='command' name='command'</td>";
+echo "<td><input list='command' id='command' name='command'";
 echo "<datalist id='command'>";
 echo "<option value='test'>";
 echo "<option value='moveTo'>";
@@ -28,7 +28,7 @@ echo "<option value='water'>";
 echo "<option value='hvec'>";
 echo "<option value='takeImage'>";
 echo "<option value='senseSoil'>";
-echo "</datalist>";
+echo "</datalist></td>";
 
 echo "<td><input type='number' id='theta' name='theta' value=0 step='any' size='2'></td>";
 echo "<td><input type='number' id='r' name='r' value=0 step='any' size='2'></td>";
