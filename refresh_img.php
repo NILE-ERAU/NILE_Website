c3c5a925aac54cf7aaf39a373701431b00b60cc9
nphaklides <nphaklides@hotmail.com>
2022-04-26 14:55:09 -0700

    extra args

--- refresh_img.php
+++ refresh_img.php
@@ -1,9 +1,9 @@
 <?php
 include("config.php");
 $sql =  "insert INTO queued_commands
-         (timestamp, command)
+         (timestamp, command, pos_theta_q, pos_r_q, pos_z_q, argument_d0, argument_d1, argument_i0)
          VALUES
-         ('2022-04-20 04:20:00.000000','takeImage');";
+         ('2022-04-20 04:20:00.000000','takeImage',0,0,0,0,0,0);";
 
 if ($link->query($sql) === TRUE) {
  echo "New record created successfully";
